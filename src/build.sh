6dfeadeee72bf74cf297b2e762afc2c21444837d
wallentx <william.allen@civitaslearning.com>
2017-09-27 15:56:57 -0500

    Updating buildfile

--- src/build.sh
+++ src/build.sh
@@ -1,26 +1,55 @@
 #!/bin/bash
 
-if [ ! $1 ]; then
-    echo "Please enter a 16-character two-step authorization key: "
-        read -e -i "$SEED" -p "MFA auth key: " SEED_INPUT
-    SEED="${SEED_INPUT:-$SEED}"
-else
-    set -- "$1" "$SEED"
-fi
+pathadd() {
+    if [ -d "$1" ] && [[ ":$PATH:" != *":$1:"* ]]; then
+        if grep -q 'PATH=$PATH:$HOME/bin' $HOME/.bash_profile; then
+            echo $HOME'/bin already added to PATH.'
+            echo 'Reload your terminal, or source '$HOME'/.bash_profile'
+        else
+            echo -e 'PATH=$PATH:$HOME/bin' >> "$HOME/.bash_profile"
+            echo 'PATH=$PATH:$HOME/bin has been added to '$HOME'/.bash_profile.'
+            echo 'Reload your terminal, or source '$HOME'/.bash_profile'
+        fi
+    fi
+}
 
-#echo "$1"
-#echo "$SEED"
+    if [ ! $1 ]; then
+        echo "Please enter a 16-character two-step authorization key: "
+            read -e -i "$SEED" -p "MFA auth key: " SEED_INPUT
+            SEED="${SEED_INPUT:-$SEED}"
+    else
+        set -- "$1" "$SEED"
+    fi
 
-if [ ! -d google-authenticator ]; then
-    https://github.com/google/google-authenticator
-else
-    #(cd google-authenticator; git pull)
-    git pull
-fi
+    if [ ! -d google-authenticator ]; then
+        git clone https://github.com/google/google-authenticator
+    else
+        #(cd google-authenticator; git pull)
+        git pull
+    fi
 
-if [ -e gmd-cmd.o ]
-then
-    rm gmd-cmd.o
-fi
+    if [ -e gmd-cmd.o ]; then
+        rm gmd-cmd.o
+    fi
 
 make all KEY="$(echo -e "$SEED" | tr '[A-Z]' '[a-z]')"
+
+    if [[ -d $HOME/bin ]]; then
+        ln -s src/bin/ga-cmd $HOME/bin/ga-cmd
+        echo -e "ga-cmd has been symlinked to $HOME/bin/ga-cmd"
+    fi
+
+    if [[ :$PATH: == *:"$HOME/bin":* ]] ; then
+        exit 0
+    else
+        echo -e "$HOME/bin exists, but is not added to PATH. Would you like to add it?"
+        read -p "Add $HOME/bin to PATH? [y/n] " -n 1 -r REPLY
+            echo
+        if [[ $REPLY =~ ^[Yy]$ ]]; then
+            pathadd $HOME/bin
+            exit 0
+        else
+            [[ $REPLY =~ ^[Nn]$ ]]
+            exit 0
+        fi
+    fi
